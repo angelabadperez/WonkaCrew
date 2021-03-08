282b579b36ddf620eeb1fc116a3e6d59c1030ed7
Ángel <angelabadperez@outlook.es>
2021-03-08 22:52:56 +0100

    Remove Pods directory

--- Pods/Target Support Files/Pods-WonkaCrew-WonkaCrewUITests/Pods-WonkaCrew-WonkaCrewUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WonkaCrew_WonkaCrewUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WonkaCrew_WonkaCrewUITests
-@end
--- Pods/Target Support Files/Pods-WonkaCrew/Pods-WonkaCrew-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WonkaCrew : NSObject
-@end
-@implementation PodsDummy_Pods_WonkaCrew
-@end
