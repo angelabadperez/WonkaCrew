1343f676019d33a1d2ebbf7095da2af9116c9432
Ángel <angelabadperez@outlook.es>
2021-03-08 22:48:56 +0100

    Added missing files

---
+++ Pods/Target Support Files/Pods-WonkaCrew-WonkaCrewUITests/Pods-WonkaCrew-WonkaCrewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WonkaCrew_WonkaCrewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WonkaCrew_WonkaCrewUITests
+@end
---
+++ Pods/Target Support Files/Pods-WonkaCrew/Pods-WonkaCrew-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WonkaCrew : NSObject
+@end
+@implementation PodsDummy_Pods_WonkaCrew
+@end
